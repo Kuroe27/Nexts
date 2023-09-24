78c091c097e5b5aac619fe613e187b1be386b9c0
Leo Mikko Uson <lancerthortan@gmail.com>
2023-09-24 13:48:04 +0800

    Added production script

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 const page = async () => {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   if (!session) {
     redirect("/api/auth/signin");
